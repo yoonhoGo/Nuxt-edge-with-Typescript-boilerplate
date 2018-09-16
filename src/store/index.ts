86da92e674b156743d1747b4f799fecde7b82d86
Yoonho Go <enoch@erion.kr>
2018-09-16 16:38:32 +0900

    vuex-class, vuex store

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import Vuex from 'vuex'
+import modules from './modules'
+
+const state = {}
+
+const mutations = {}
+
+const actions = {}
+
+export default () => new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  modules,
+})
